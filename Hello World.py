d17197badc00eab5ee1fbce444b726ad00e394ec
Jasraj2904 <jasrajsinghchatha@gmail.com>
2026-04-17 18:58:58 +0530

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,11 @@
+print("Hello, I am a Chatbot")
+x = input()
+print("Hello , {input}")
+print("How are you doing?")
+y = input.lower()
+if y == "good":
+    print("That's great to hear!")
+elif y == "bad":
+    print("I'm sorry to hear that. I hope things get better for you soon.")
+else:    print("I see. Thanks for sharing that with me.")
+print("It was nice talking to you. Have a great day!")
